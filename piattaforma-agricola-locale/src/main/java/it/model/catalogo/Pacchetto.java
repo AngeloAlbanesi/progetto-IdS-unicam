095599bcdcde8e192353aec6fed9aef05adc2767
paolocampanari <paolo.campanari@studenti.unicam.it>
2025-05-08 16:55:18 +0200

    creazione classe evento

--- piattaforma-agricola-locale/src/main/java/it/model/catalogo/Pacchetto.java
+++ piattaforma-agricola-locale/src/main/java/it/model/catalogo/Pacchetto.java
@@ -3,4 +3,30 @@ package it.model.catalogo;
 import it.model.common.Acquistabile;
 
 public class Pacchetto implements Acquistabile {
+    private String idPacchetto;
+    private String nome;
+    private String descrizione;
+    private double prezzoPacchetto;
+    private List <Acquistabile> elementiInclusi;
+    private DistributoreDiTipicita distributore;
+
+    public Pacchetto(String idPacchetto, String nome, String descrizione, double prezzoPacchetto,
+            List <Acquistabile> elementiInclusi, DistributoreDiTipicita distributore) {
+        this.idPacchetto = idPacchetto;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzoPacchetto = prezzoPacchetto;
+        this.elementiInclusi = elementiInclusi;
+        this.distributore = distributore;
+    }
+
+    public String getId() {
+        return idPacchetto;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getDescrizione() {
+        return descrizione;
+    }
 }
